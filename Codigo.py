865127dbabb8b1c2fc8d8122bb2b2a76d8cbe82b
VitorAndrade07 <uchihavitor16@gmail.com>
2024-12-04 13:05:15 -0300

    .

--- Codigo.py
+++ Codigo.py
@@ -1,8 +1,7 @@
-from vingadores import Interface
-from vingadores import Vingador
+from  model.vingadores import Interface
+from  model.vingadores import Vingador
 
 def main():
-    Vingador('Homem de Ferro', 'Tony Stark', 'Humano', ['Inteligência', 'Engenharia', 'Rico'], 'Armadura', ['Arrogância', 'Orgulho'], 2000)
     Vingador('Capitão América', 'Steve Rogers', 'Humano', ['Força', 'Habilidade em Combate', 'Liderança'], 'Escudo', ['Inexperiência no mundo moderno'], 8000)
     Vingador('Star-Lord', 'Peter Quill', 'Humano', ['Liderança', 'Habilidade em Combate', 'Inteligência'], 'Pistolas e Jetpack', ['Impulsividade'], 7500)
     Vingador('Hulk', 'Bruce Banner', 'Humano', ['Força sobre-humana', 'Regeneração', 'Durabilidade'], 'Força', ['Raiva', 'Controle mental'], 10000)
